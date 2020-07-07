0a471b443e7f8da70550abdfd92279d92eb5ac22
jonathanTellechea <jtellece@ucsc.edu>
2020-07-06 20:54:18 -0700

    adding backgrounds

--- nnKeras.py
+++ nnKeras.py
@@ -1,3 +1,7 @@
+# Written By : Jonathan O. Tellechea
+# Adviser    : Mike Hance, Phd
+# Research   : Using a neural network to maximize the significance of tttHH production.
+# Reference  :http://cdsweb.cern.ch/record/2220969/files/ATL-PHYS-PUB-2016-023.pdf
 import numpy as np
 import uproot
 import tensorflow as tf
@@ -5,6 +9,10 @@ import matplotlib.pyplot as plt
 from keras.layers import Input, Flatten, Dense, Dropout
 from keras.models import Model
 
-events = uproot.open('new_signal_tthh.root')['OutputTree']
-events.keys()
+tree = 'OutputTree'
 
+signal  = uproot.open('new_signal_tthh.root')[tree]
+bg_ttZ  = uproot.open('new_background_ttZ.root')[tree]
+bg_ttbb = uproot.open('new_background_ttbb.root')[tree]
+bg_ttH  = uproot.open('new_background_ttH.root')[tree]
+background = np.concatenate((bg_ttbb,bg_ttH,bg_ttZ))
