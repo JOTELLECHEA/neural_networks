57b78cdc65022e7c5a7fbe5affb954e2c60ad45d
JOTELLECHEA <jtellece@ucsc.edu>
2020-09-25 22:01:10 -0700

    changed paramters

--- hyperparameterRecord.py
+++ hyperparameterRecord.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-# pd.options.display.float_format = '${:,.2f}'.format
-file = 'hyperparameterRecord.csv'
-modelParam  = ['NN Archi.','#Br.','LR','Batch','#Layers','#Neurons','#Epochs','#EpochsRan','AUC','Sigif.','File']
+file = 'testing.csv'
+modelParam  = ['NN Archi.','#Br.','LR','Batch','AUC','Avg.P','Y/M/D @ H:M','ConfusionMatrix [TP FP] [FN TN]','Score','Max Signif','nsig','nbkg']
 data = pd.read_csv(file)
 print(data.to_string(justify='left',columns=modelParam,header=True,index=False))
