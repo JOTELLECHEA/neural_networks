5f0aa6bec6eefee1bccdd7c7cf136e5f1e9e9cbe
JOTELLECHEA <jtellece@ucsc.edu>
2020-11-24 21:23:21 -0800

    passed new file

--- hyperparameterRecord.py
+++ hyperparameterRecord.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 # file = 'hyperparameterRecord_v3.csv'
-file = 'fiveLayerDropout.csv'
+file = 'fiveLayerDropout_1.csv'
 # modelParam  = ['NN Archi.','#Br.','LR','Batch','AUC','Avg.P','Y/M/D @ H:M','ConfusionMatrix [TP FP] [FN TN]','Score','Max Signif','nsig','nbkg']
 modelParam  = ['NN Archi.','#Br.','LR','Batch','AUC','Avg.P','Run Time','ConfusionMatrix [TP FP] [FN TN]','Score','Max Signif','nsig','nbkg']
 data = pd.read_csv(file)
