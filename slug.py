978eb8c7009b260e97a5522bc50a8da163dd1ddd
JOTELLECHEA <jtellece@ucsc.edu>
2020-11-27 01:51:56 -0800

    formatted with black

--- slug.py
+++ slug.py
@@ -1,29 +1,32 @@
 import tkinter as tk
 import math
 import matplotlib
-matplotlib.use('TkAgg')
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
-def plotPR(x,y,t):
+
+def plotPR(x, y, t):
     plt.subplot(411)
-    plt.plot(t,x[:-1],'b--', label = 'Precision')
-    plt.plot(t,y[:-1],'g-', label = 'Recall')
+    plt.plot(t, x[:-1], "b--", label="Precision")
+    plt.plot(t, y[:-1], "g-", label="Recall")
     plt.ylim([0.00, 1.05])
-    plt.xlabel('Threshold')
-    plt.title('Precision/Recall vs. Threshold Curve')
-    plt.legend(loc = 'lower right')
+    plt.xlabel("Threshold")
+    plt.title("Precision/Recall vs. Threshold Curve")
+    plt.legend(loc="lower right")
     plt.grid()
 
-def plotROC(x,y,AUC):
+
+def plotROC(x, y, AUC):
     plt.subplot(412)
-    plt.plot(x,y, lw = 1, label = 'ROC (area = %0.6f)'%(AUC))
-    plt.plot([0, 1], [0, 1], '--', color = (0.6, 0.6, 0.6), label = 'Luck')
+    plt.plot(x, y, lw=1, label="ROC (area = %0.6f)" % (AUC))
+    plt.plot([0, 1], [0, 1], "--", color=(0.6, 0.6, 0.6), label="Luck")
     plt.xlim([-0.05, 1.05])
     plt.ylim([-0.05, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic')
-    plt.legend(loc = 'lower right')
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Receiver operating characteristic")
+    plt.legend(loc="lower right")
     plt.grid()
 
 
@@ -42,33 +45,35 @@ def getZPoisson(s, b, stat, syst):
     characterizing the uncertainty due to limited MC statistics used
     to estimate the background yield.
     """
-    n = s+b
+    n = s + b
 
     # this is a relative uncertainty
-    sigma = math.sqrt(stat**2+syst**2)
+    sigma = math.sqrt(stat ** 2 + syst ** 2)
 
     # turn into the total uncertainty
-    sigma=sigma*b
+    sigma = sigma * b
 
     if s <= 0 or b <= 0:
         return 0
 
-    factor1=0
-    factor2=0
+    factor1 = 0
+    factor2 = 0
 
-    if (sigma < 0.01):
-        #In the limit where the total BG uncertainty is zero,
-        #this reduces to approximately s/sqrt(b)
-        factor1 = n*math.log((n/b))
-        factor2 = (n-b)
+    if sigma < 0.01:
+        # In the limit where the total BG uncertainty is zero,
+        # this reduces to approximately s/sqrt(b)
+        factor1 = n * math.log((n / b))
+        factor2 = n - b
     else:
-        factor1 = n*math.log( (n*(b+sigma**2))/((b**2)+n*sigma**2) )
-        factor2 = ((b**2)/(sigma**2))*math.log( 1 + ((sigma**2)*(n-b))/(b*(b+sigma**2)) )
+        factor1 = n * math.log((n * (b + sigma ** 2)) / ((b ** 2) + n * sigma ** 2))
+        factor2 = ((b ** 2) / (sigma ** 2)) * math.log(
+            1 + ((sigma ** 2) * (n - b)) / (b * (b + sigma ** 2))
+        )
 
-    signif=0
+    signif = 0
     try:
-        signif=math.sqrt( 2 * (factor1 - factor2))
+        signif = math.sqrt(2 * (factor1 - factor2))
     except ValueError:
-        signif=0
+        signif = 0
 
     return signif
