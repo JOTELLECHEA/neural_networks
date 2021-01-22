af7a1b9265df29e3cef1e38a06cb26091d3519cc
JOTELLECHEA <jtellece@ucsc.edu>
2021-01-21 21:58:34 -0800

    change

--- multiNN.py
+++ multiNN.py
@@ -1,7 +1,21 @@
 import nnKerasGPU as nn
 
-for i in range(3,15):
-    nn.main(i,512,0)
+batch = 512
+
+i =5
+nn.main(i,batch,0.5)
+nn.main(i,256,0.5)
+nn.main(i,128,0.5)
+nn.main(i,64,0.5)
+nn.main(i,32,0.5)
+# nn.main(i,batch,0.4)
+# nn.main(i,batch,0.35)
+# nn.main(i,batch,0.3)
+# nn.main(i,batch,0.25)
+# nn.main(i,batch,0.2)
+# nn.main(i,batch,0.15)
+# nn.main(i,batch,0.1)
+# nn.main(i,batch,0.05)
 
 
 
