5a6694cecde542f88f27b07e1dec458d27deec20
JOTELLECHEA <jtellece@ucsc.edu>
2021-01-22 13:18:33 -0800

    fixed

--- hyperparameterRecord.py
+++ hyperparameterRecord.py
@@ -4,16 +4,16 @@ import argparse
 
 
 
-parser = argparse.ArgumentParser(description="Plot 1D plots of sig/bac")
-parser.add_argument("--file", type=str, help="Use '--file=' followed by a *.h5 file")
-args = parser.parse_args()
-file = "data/" + str(args.file)
+# parser = argparse.ArgumentParser(description="Plot 1D plots of sig/bac")
+# parser.add_argument("--file", type=str, help="Use '--file=' followed by a *.h5 file")
+# args = parser.parse_args()
+# file = "data/" + str(args.file)
 
 
 # file = 'hyperparameterRecord_v3.csv'
 # file = 'fiveLayerDropout_2.csv'
 # file = 'fiveLayerDropout_3.csv'
-file = 'testelep2.csv'
+file = 'csv/testelep2.csv'
 # modelParam  = ['NN Archi.','#Br.','LR','Batch','AUC','Avg.P','Y/M/D @ H:M','ConfusionMatrix [TP FP] [FN TN]','Score','Max Signif','nsig','nbkg']
 # modelParam  = ['NN Archi.','#Br.','LR','Batch','AUC','Avg.P','Run Time','ConfusionMatrix [TP FP] [FN TN]','Score','Max Signif','nsig','nbkg']
 modelParam = [
