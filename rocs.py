05fbc815cbfe711875dd50621c3ec106dafac939
JOTELLECHEA <jtellece@ucsc.edu>
2021-01-21 22:02:18 -0800

    feature short

--- rocs.py
+++ rocs.py
@@ -21,7 +21,7 @@ from tensorflow.keras.models import load_model
 from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import auc
+from sklearn.metrics import auc,roc_curve
 
 sc = StandardScaler()
 seed = 42
@@ -29,19 +29,19 @@ tree = "OutputTree"
 
 # Branches names of high/low level variables aka: features.
 HighLevel = [
-    # "numjet",
-    # "numlep",
-    # "btag",
-    # "srap",
-    # "cent",
-    # "m_bb",
-    # "h_b",
-    # "mt1",
-    # "mt2",
-    # "mt3",
-    # "dr1",
-    # "dr2",
-    # "dr3",
+    "numjet",
+    "numlep",
+    "btag",
+    "srap",
+    "cent",
+    "m_bb",
+    "h_b",
+    "mt1",
+    "mt2",
+    "mt3",
+    "dr1",
+    "dr2",
+    "dr3",
 ]
 type = ['flav','pT','eta','phi','b','c']
 LeptonVAR = []
@@ -53,7 +53,8 @@ for i in range(1,6):
     for j in range(10):
         JetVAR.append('jet'+ str(j+1) + type[i])
 
-branches = sorted(HighLevel + JetVAR + LeptonVAR+ ["weights"])
+# branches = sorted(HighLevel + JetVAR + LeptonVAR+ ["weights"])
+branches = sorted(HighLevel + ['weights'])
 numBranches = len(branches) - 1
 
 parser = argparse.ArgumentParser(description="Plot 1D plots of sig/bac")
@@ -84,75 +85,76 @@ bkgw = rawdata["weights"][len(signal) :]
 # Labeling data with 1's and 0's to distinguish.
 y = np.concatenate((np.ones(len(signal)), np.zeros(len(shuffleBackground))))
 
-# Shuffle full data and split into train/test and validation set.
-X_dev, X_eval, y_dev, y_eval = train_test_split(
-    X, y, test_size=0.001, random_state=seed
-)
-X_train, X_test, y_train, y_test = train_test_split(
-    X_dev, y_dev, test_size=0.2, random_state=seed
-)
+# # Shuffle full data and split into train/test and validation set.
+# X_dev, X_eval, y_dev, y_eval = train_test_split(
+#     X, y, test_size=0.001, random_state=seed
+# )
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X_dev, y_dev, test_size=0.2, random_state=seed
+# )
 
 neuralNet = keras.models.load_model(file)
 
-allScore = neuralNet.predict(X)
+y_predicted = neuralNet.predict(X)
+fp, tp, thresholds = roc_curve(y, y_predicted)
 
 flag2 = 1
 if flag2 == 1:
     numbins = 100000
 
-    sigScore = neuralNet.predict(X[y > 0.5]).ravel()
-    bkgScore = neuralNet.predict(X[y < 0.5]).ravel()
-    sigSUM = len(sigScore)
-    bkgSUM = len(bkgScore)
+    # sigScore = neuralNet.predict(X[y > 0.5]).ravel()
+    # bkgScore = neuralNet.predict(X[y < 0.5]).ravel()
+    # sigSUM = len(sigScore)
+    # bkgSUM = len(bkgScore)
 
-    xlimit = (0, 1)
-    tp = []
-    fp = []
-    hist, bins = np.histogram(sigScore, bins=numbins, range=xlimit, density=False)
-    count = 0
-    for i in range(numbins - 1, -1, -1):
-        count += hist[i] / sigSUM
-        tp.append(count)
-    hist, bins = np.histogram(bkgScore, bins=numbins, range=xlimit, density=False)
-    count = 0
-    for j in range(numbins - 1, -1, -1):
-        count += hist[j] / bkgSUM
-        fp.append(count)
-    area = auc(fp, tp)
-    xplot = tp
-    yplot = fp
-    # computes max signif
-    sigSUM = len(sigScore) * scalefactor
-    tp = np.array(tp) * sigSUM
-    fp = np.array(fp) * bkgSUM
-    syst = 0.0
-    stat = 0.0
-    maxsignif = 0.0
-    maxs = 0
-    maxb = 0
-    bincounter = numbins - 1
-    bincountatmaxsignif = 999
-    bkgRegection = []
-    for t, f in zip(tp, fp):
-        bkgRegection.append(1/f)
-        signif = slug.getZPoisson(t, f, stat, syst)
-        if f >= 10 and signif > maxsignif:
-            maxsignif = signif
-            maxs = t
-            maxb = f
-            bincountatmaxsignif = bincounter
-            score = bincountatmaxsignif / numbins
-        bincounter -= 1
-    print(
-        "\n Score = %6.3f\n Signif = %5.2f\n nsig = %d\n nbkg = %d\n"
-        % (score, maxsignif, maxs, maxb)
-    )
-    data = {'xplot':xplot,'yplot':yplot,'bkgR':bkgRegection,'maxsignif':maxsignif,'area':area}
+    # xlimit = (0, 1)
+    # tp = []
+    # fp = []
+    # hist, bins = np.histogram(sigScore, bins=numbins, range=xlimit, density=False)
+    # count = 0
+    # for i in range(numbins - 1, -1, -1):
+    #     count += hist[i] / sigSUM
+    #     tp.append(count)
+    # hist, bins = np.histogram(bkgScore, bins=numbins, range=xlimit, density=False)
+    # count = 0
+    # for j in range(numbins - 1, -1, -1):
+    #     count += hist[j] / bkgSUM
+    #     fp.append(count)
+    # area = auc(fp, tp)
+    # xplot = tp
+    # yplot = fp
+    # # computes max signif
+    # sigSUM = len(sigScore) * scalefactor
+    # tp = np.array(tp) * sigSUM
+    # fp = np.array(fp) * bkgSUM
+    # syst = 0.0
+    # stat = 0.0
+    # maxsignif = 0.0
+    # maxs = 0
+    # maxb = 0
+    # bincounter = numbins - 1
+    # bincountatmaxsignif = 999
+    # bkgRegection = []
+    # for t, f in zip(tp, fp):
+    #     bkgRegection.append(1/f)
+    #     signif = slug.getZPoisson(t, f, stat, syst)
+    #     if f >= 10 and signif > maxsignif:
+    #         maxsignif = signif
+    #         maxs = t
+    #         maxb = f
+    #         bincountatmaxsignif = bincounter
+    #         score = bincountatmaxsignif / numbins
+    #     bincounter -= 1
+    # print(
+    #     "\n Score = %6.3f\n Signif = %5.2f\n nsig = %d\n nbkg = %d\n"
+    #     % (score, maxsignif, maxs, maxb)
+    # )
+    data = {'xplot':fp,'yplot':tp,'bkgR':1/fp}
  
 
     df = pd.DataFrame(data)
 
-    # df.to_csv("highlvlvars.csv", mode="a", header=True, index=False)
+    df.to_csv("highlvlvars.csv", mode="a", header=True, index=False)
     # df.to_csv("highandlowlvlvars.csv", mode="a", header=True, index=False)
-    df.to_csv("lowlvlvars.csv", mode="a", header=True, index=False)
+    # df.to_csv("lowlvlvars.csv", mode="a", header=True, index=False)
 
