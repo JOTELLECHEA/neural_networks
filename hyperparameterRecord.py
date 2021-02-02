cfcd05db88d1bb2fde7935fe309747382917c256
JOTELLECHEA <jtellece@ucsc.edu>
2021-02-02 04:24:55 -0800

    commnet

--- hyperparameterRecord.py
+++ hyperparameterRecord.py
@@ -1,21 +1,28 @@
+# Written By : Jonathan O. Tellechea
+# Adviser    : Mike Hance, Phd
+# Research   : Using a neural network to maximize the significance of tttHH production.
+# Description: This script keeps a record of trained NN; Keeps track of time , AUC , lenght of NN etc. 
+# The filename for the saved weights is displayed to be used in loadNN.py to create plots.
+# Reference  :http://cdsweb.cern.ch/record/2220969/files/ATL-PHYS-PUB-2016-023.pdf
+###########################################################################################################################
+# Imported packages.
 import pandas as pd
 import numpy as np
 import argparse
 
 
-
+####work in progress to automate script
 # parser = argparse.ArgumentParser(description="Plot 1D plots of sig/bac")
 # parser.add_argument("--file", type=str, help="Use '--file=' followed by a *.h5 file")
 # args = parser.parse_args()
 # file = "data/" + str(args.file)
-
-
 # file = 'hyperparameterRecord_v3.csv'
 # file = 'fiveLayerDropout_2.csv'
 # file = 'fiveLayerDropout_3.csv'
-file = 'csv/testelep2.csv'
 # modelParam  = ['NN Archi.','#Br.','LR','Batch','AUC','Avg.P','Y/M/D @ H:M','ConfusionMatrix [TP FP] [FN TN]','Score','Max Signif','nsig','nbkg']
-# modelParam  = ['NN Archi.','#Br.','LR','Batch','AUC','Avg.P','Run Time','ConfusionMatrix [TP FP] [FN TN]','Score','Max Signif','nsig','nbkg']
+# modelParam  = ['NN Archi.','#Br.','LR','Batch','AUC','Avg.P','Run Time','ConfusionMatrix [TP FP] [FN TN]','Score','Max Signif','nsig','nbkg']#######
+
+file = 'csv/testelep2.csv'
 modelParam = [
         'FileName',
         "ConfusionMatrix [TP FP] [FN TN]",
