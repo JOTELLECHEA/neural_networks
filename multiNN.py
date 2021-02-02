a42f5550f2eeaade2909502bc634f0c0b41ee5d4
JOTELLECHEA <jtellece@ucsc.edu>
2021-02-02 04:26:51 -0800

    added comments

--- multiNN.py
+++ multiNN.py
@@ -1,3 +1,10 @@
+# Written By : Jonathan O. Tellechea
+# Adviser    : Mike Hance, Phd
+# Research   : Using a neural network to maximize the significance of tttHH production.
+# Description: This will repeat nnKerasGPU.py multiple times to run it multiple times unsupervised. Must be edited to run scenario of interest.
+# Reference  :http://cdsweb.cern.ch/record/2220969/files/ATL-PHYS-PUB-2016-023.pdf
+###########################################################################################################################
+# Imported packages.
 import nnKerasGPU as nn
 
 batch = 512
